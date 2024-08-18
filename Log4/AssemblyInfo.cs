e372247688429cc3f40b23dd07fe3271b2657093
Jeyhun-Aghayev <bd8ez2q6v@code.edu.az>
2024-08-18 22:55:36 +0400

    Çakışmalar çözüldü

---
+++ Log4/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4
+{
+    public class AssemblyInfo
+    {
+        /*AssemblyInfo.cs Faylında Log4net-i Konfiqurasiya Etmək
+        Log4net konfiqurasiya faylını layihəyə tanıtmaq üçün AssemblyInfo.cs faylınıza aşağıdakı sətiri əlavə edin. Bu addım log4net-ə konfiqurasiya faylını izləməyi bildirir:
+        */
+        [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+
+        //Konfiqurasiya Faylının Yaradılması
+
+    }
+}
